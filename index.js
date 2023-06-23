64f2e93bf10b1c1795940a41482bcd44e78645da
wayne <keyawayne1@gmail.com>
2023-06-23 22:49:46 +0300

    assinment done

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function(){
+    const textparagraph = document.getElementById("text");
+    textparagraph.textContent = "This is really cool!";
+    });
